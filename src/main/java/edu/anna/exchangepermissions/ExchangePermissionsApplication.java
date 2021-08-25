4da5edb7242598ea921571661f34fe4fd83a1ca7
Anna Masdeu <anna.masdeu@thoughtworks.com>
2021-08-25 16:41:54 +0200

    Adds skeleton of poject

---
+++ src/main/java/edu/anna/exchangepermissions/ExchangePermissionsApplication.java
@@ -0,0 +1,13 @@
+package edu.anna.exchangepermissions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangePermissionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangePermissionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/anna/exchangepermissions/ExchangePermissionsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.anna.exchangepermissions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangePermissionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
