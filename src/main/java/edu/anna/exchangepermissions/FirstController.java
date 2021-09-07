b2da156fffc5874ccb6922f78d8b525e463f3de0
Anna Masdeu <anna.masdeu@thoughtworks.com>
2021-09-07 18:05:14 +0200

    Adds first controller skeleton

---
+++ src/main/java/edu/anna/exchangepermissions/FirstController.java
@@ -0,0 +1,17 @@
+package edu.anna.exchangepermissions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("v1")
+public class FirstController {
+
+    @GetMapping("/something")
+    public ResponseEntity<HttpStatus> getSomething() {
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
