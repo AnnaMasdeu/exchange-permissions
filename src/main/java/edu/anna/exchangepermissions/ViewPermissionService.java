a96d1f615d418f6b77ed39b12bf884c6db4ba66d
Anna Masdeu <anna.masdeu@thoughtworks.com>
2021-09-27 14:30:43 +0200

    Implements getPermission service method

--- src/main/java/edu/anna/exchangepermissions/ViewPermissionService.java
+++ src/main/java/edu/anna/exchangepermissions/ViewPermissionService.java
@@ -2,13 +2,23 @@ package edu.anna.exchangepermissions;
 
 import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
 @Component
 public class ViewPermissionService {
 
-    private ViewPermissionRepository viewPermissionRepository;
+    private final ViewPermissionRepository viewPermissionRepository;
+
+    public ViewPermissionService(ViewPermissionRepository viewPermissionRepository) {
+        this.viewPermissionRepository = viewPermissionRepository;
+    }
 
     public ViewPermission getPermission(String exchangeId, String accountId) {
-        return ViewPermission.OFF;
+        UserPermissionId userPermissionId = new UserPermissionId(accountId, exchangeId);
+
+        Optional<UserPermission> viewPermission = viewPermissionRepository.findById(userPermissionId);
+        return viewPermission.map(UserPermission::getViewPermission)
+                .orElse(ViewPermission.OFF);
     }
 
     public void changePermission(String exchangeId, String accountId, ViewPermission viewPermission) {
---
+++ src/test/java/edu/anna/exchangepermissions/ViewPermissionServiceTest.java
@@ -0,0 +1,42 @@
+package edu.anna.exchangepermissions;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.*;
+
+class ViewPermissionServiceTest {
+
+    private static final String ACCOUNT_ID = "123";
+    private static final String EXCHANGE_ID = "LSE";
+
+    private final ViewPermissionRepository repository = mock(ViewPermissionRepository.class);
+    private final ViewPermissionService service = new ViewPermissionService(repository);
+
+    @Test
+    void shouldGetPermissionCorrectly() {
+        UserPermissionId permissionId = new UserPermissionId(ACCOUNT_ID, EXCHANGE_ID);
+        UserPermission userPermission = new UserPermission(permissionId, ViewPermission.L1);
+        when(repository.findById(permissionId)).thenReturn(Optional.of(userPermission));
+
+        ViewPermission actualViewPermission = service.getPermission(EXCHANGE_ID, ACCOUNT_ID);
+
+        verify(repository).findById(permissionId);
+        assertThat(actualViewPermission, equalTo(ViewPermission.L1));
+    }
+
+    @Test
+    void shouldGetPermissionOffByDefault() {
+        UserPermissionId permissionId = new UserPermissionId(ACCOUNT_ID, EXCHANGE_ID);
+        when(repository.findById(permissionId)).thenReturn(Optional.empty());
+
+        ViewPermission actualViewPermission = service.getPermission(EXCHANGE_ID, ACCOUNT_ID);
+
+        verify(repository).findById(permissionId);
+        assertThat(actualViewPermission, equalTo(ViewPermission.OFF));
+    }
+
+}
