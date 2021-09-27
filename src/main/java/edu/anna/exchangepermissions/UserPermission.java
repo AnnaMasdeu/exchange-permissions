eb28ba03e5df141324451c8710fd56c483b008a5
Anna Masdeu <anna.masdeu@thoughtworks.com>
2021-09-27 14:28:08 +0200

    Introduces UserPermission and UserPermissionId entities

--- src/main/java/edu/anna/exchangepermissions/UserPermission.java
+++ src/main/java/edu/anna/exchangepermissions/UserPermission.java
@@ -1,7 +1,13 @@
 package edu.anna.exchangepermissions;
 
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import java.util.Objects;
+
+@Entity
 public class UserPermission {
 
+    @EmbeddedId
     private UserPermissionId userPermissionId;
     private ViewPermission viewPermission;
 
@@ -16,5 +22,18 @@ public class UserPermission {
 
     public ViewPermission getViewPermission() {
         return viewPermission;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPermission that = (UserPermission) o;
+        return Objects.equals(userPermissionId, that.userPermissionId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userPermissionId);
     }
 }
--- src/main/java/edu/anna/exchangepermissions/UserPermissionId.java
+++ src/main/java/edu/anna/exchangepermissions/UserPermissionId.java
@@ -1,9 +1,17 @@
 package edu.anna.exchangepermissions;
 
+import javax.persistence.Embeddable;
+import java.util.Objects;
+
+@Embeddable
 public class UserPermissionId {
 
     private String accountId;
     private String exchangeId;
+
+    private UserPermissionId() {
+
+    }
 
     public UserPermissionId(String accountId, String exchangeId) {
         this.accountId = accountId;
@@ -16,5 +24,18 @@ public class UserPermissionId {
 
     public String getExchangeId() {
         return exchangeId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserPermissionId that = (UserPermissionId) o;
+        return Objects.equals(accountId, that.accountId) && Objects.equals(exchangeId, that.exchangeId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(accountId, exchangeId);
     }
 }
